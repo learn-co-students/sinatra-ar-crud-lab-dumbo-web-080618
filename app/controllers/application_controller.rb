eaa7ebeed9d95d7a873f012911ab38a356842cc1
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2018-08-28 20:01:45 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -9,6 +10,44 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
   end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    @post = Post.create(name:params[:name], content: params[:content])
+    redirect to ("/posts")
+  end
+
+  get '/posts/:id' do  #loads show page
+    @post = Post.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do #loads edit form
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do  #updates a post
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    # erb :show
+    redirect to("posts/#{@post.id}")
+  end
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    #binding.pry
+    @post.destroy
+  end
+
 end
