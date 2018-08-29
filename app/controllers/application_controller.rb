e1eea20c610f3d9326c1412f4b4ebea6dca67a10
mikepcarlos <michaelpcarlos@gmail.com>
2018-08-29 15:57:23 -0400

    sinatra ar crud

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,44 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    "Hello!"
+  end
 
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  post '/posts' do
+    @post = Post.create(params)
+    #once you make something new or delete you redirect (usually)
+    redirect to '/posts'
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.update(name: params[:name], content: params[:content])
+    redirect to "/posts/#{@post.id}"
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    erb :delete
   end
 end
